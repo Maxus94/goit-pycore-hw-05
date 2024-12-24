f0dac5d2f86df0b7380000d1de68c43a073f8740
Maxim <kmaxusk@gmail.com>
2024-12-24 20:28:50 +0200

    task_3_completed

--- task_3.py
+++ task_3.py
@@ -1,37 +1,44 @@
-import sys, collections, re
+import sys, collections
 from pathlib import Path
-from datetime import datetime
 
-file = "text.log"
-log_file = "text.log"
-log_lvl = "ERROR"
+def file_error(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except TypeError as e:
+            print(e)
+            sys.exit()
+        
+        except FileNotFoundError as e:
+            print(e)
+            sys.exit()      
+    return inner
 
+@file_error
 def load_logs(file_path: str) -> list:
     """
     Create list of log records from log file
     """    
     p = Path(file_path)
     if not p.exists():        
-        # raise FileNotFoundError(f"File {sys.argv[1]} not found")
-        raise FileNotFoundError(f"File not found")
+        raise FileNotFoundError(f"File {sys.argv[1]} not found")
     with open(file_path, 'r', encoding='utf-8') as file:
         file_content = file.readlines()
         logs=[]            
         for line in file_content:
-            line = line.strip()                            
-            logs.append(parse_log_line(line))                                       
+            line = line.strip()                          
+            logs.append(parse_log_line(line))            
     return logs
 
+@file_error
 def parse_log_line(line: str) -> dict:
     """
     Convert log record to dictionary of it's properties
     """    
     line = line.replace(" ", "|", 3)
-    line = line.split('|')
-    # print(len(line)!=4)
+    line = line.split('|')    
     if len(line) != 4:        
-        # raise TypeError(f"File of logs {sys.argv[1]} has wrong format")        
-        raise TypeError(f"File of logs has wrong format")        
+        raise TypeError(f"File of logs {sys.argv[1]} has wrong format")              
     return {'date': line[0], 'time': line[1], 'level': line[2].upper(), 'text': line[3]}
 
 def filter_logs_by_level(logs: list, level: str) -> list:
@@ -63,23 +70,24 @@ def display_log_counts(counts: dict):
     return
 
 levels = []
-# logs_list = load_logs(sys.argv[1])
-logs_list = load_logs(file)
 
-display_log_counts(count_logs_by_level(logs_list))
-# if len(sys.argv) == 3:
-if len(log_file) != 0:
+if len(sys.argv) < 2:
+    """
+    Stop program if parameters were not given
+    """        
+    print("There is no parameters, need log file address")
+    sys.exit()
+
+display_log_counts(count_logs_by_level(load_logs(sys.argv[1])))
+
+if len(sys.argv) > 2:
     """
     Print all log records of level if second parameter of command line is exist
     """    
-    # if sys.argv[2].upper() not in set(levels):
-    if log_lvl.upper() not in set(levels):
-        print(f"\nЛоги не знайдено, у файлі є такі рівні: {set(levels)}")
-        # print(f"\nЛоги для рівня '{sys.argv[2].upper()}' не знайдено, у файлі {sys.argv[1]} є такі рівні: {set(levels)}")
-    else:
-        logs = filter_logs_by_level(logs_list, log_lvl)
-        # logs = filter_logs_by_level(logs_list, sys.argv[2])            
-        # print(f"\nДеталі логів для рівня '{sys.argv[2].upper()}':")
-        print(f"\nДеталі логів:")
+    if sys.argv[2].upper() not in set(levels):    
+        print(f"\nЛоги для рівня '{sys.argv[2].upper()}' не знайдено, у файлі {sys.argv[1]} є такі рівні: {set(levels)}")
+    else:        
+        logs = filter_logs_by_level(load_logs(sys.argv[1]), sys.argv[2])            
+        print(f"\nДеталі логів для рівня '{sys.argv[2].upper()}':")
         for log in logs:        
             print(f"{log['date']} {log['time']} - {log['text']}")
